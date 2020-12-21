5900136be3e7709489e19dc06cea85b6d70d6563
haitamlamtai <haitam.lamtai@gmail.com>
2020-12-21 14:14:38 +0100

    plot 2

--- plot2.R
+++ plot2.R
@@ -3,8 +3,11 @@
 datetime <- paste(as.Date(subdata$Date), subdata$Time)
 subdata$Datetime <- as.POSIXct(datetime)
 
+#plott
+png(file="plot2.png",width = 480,height = 480)
+
 plot(subdata$Global_active_power ~ data$Datetime, type = "l",
      ylab = "Global Active Power (kilowatts)", xlab = "")
 
 
-with(data = subdata,plot(y = Global_reactive_power,x=
+dev.off()
