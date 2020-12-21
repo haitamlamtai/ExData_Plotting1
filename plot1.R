84036600ddd0ebe82801dabb28f94c6f2799d676
haitamlamtai <haitam.lamtai@gmail.com>
2020-12-21 14:17:01 +0100

    plot and code 1

--- plot1.R
+++ plot1.R
@@ -1,6 +1,6 @@
 
 png(file="plot1.png",width = 480,height = 480)
-with(data = subdata,hist(Global_reactive_power,col="red",breaks = 12
+with(data = subdata,hist(Global_active_power,col="red",breaks = 12
                          ,main = "Global Active Power",
                          xlab="Global Active Power (kilowatts)"))
 dev.off()
